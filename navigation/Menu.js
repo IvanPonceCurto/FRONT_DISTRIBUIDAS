03806b71e75814464e61720e0a0e7b6cb73e1fe6
Fran Fares <franciscofares23@gmail.com>
2021-04-23 09:38:04 -0300

    entendiendo el codigo

---
+++ navigation/Menu.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { useSafeArea } from "react-native-safe-area-context";
+import {
+  ScrollView,
+  StyleSheet,
+  Image
+} from "react-native";
+import { Block, Text, theme } from "galio-framework";
+
+import Images from "../constants/Images";
+import { DrawerItem as DrawerCustomItem } from '../components';
+
+function CustomDrawerContent({ drawerPosition, navigation, profile, focused, state, ...rest }) {
+  const insets = useSafeArea();
+  const screens = [
+    "Home", 
+    "Profile",
+    "Account",
+    "Elements",
+    "Articles",
+  ];
+
+  return (
+    <Block
+      style={styles.container}
+      forceInset={{ top: 'always', horizontal: 'never' }}
+    >
+      <Block flex={0.06} style={styles.header}>
+        <Image style={{width:300}} source={Images.Menu} />
+      </Block>
+      <Block flex style={{ paddingLeft: 8, paddingRight: 14 },styles.content}>
+        <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+          {screens.map((item, index) => {
+              return (
+                <DrawerCustomItem
+                  title={item}
+                  key={index}
+                  navigation={navigation}
+                  focused={state.index === index ? true : false}
+                />
+              );
+            })}
+            <Block flex style={{ marginTop: 24, marginVertical: 8, paddingHorizontal: 8 }}>
+              <Block style={{ borderColor: "rgba(0,0,0,0.2)", width: '100%', borderWidth: StyleSheet.hairlineWidth }}/>
+              <Text color="#8898AA" style={{ marginTop: 16, marginLeft: 8 }}>DOCUMENTATION</Text>
+            </Block>
+            <DrawerCustomItem title="Getting Started" navigation={navigation} />
+        </ScrollView>
+      </Block>
+    </Block>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    paddingBottom: theme.SIZES.BASE * 2,
+    shadowColor: theme.COLORS.BLACK,
+    
+    shadowRadius: 10,
+    shadowOpacity: 0.75,
+    elevation: 2
+    
+  },
+  content:{
+    paddingHorizontal: 28,
+    paddingTop: theme.SIZES.BASE * 3,
+    justifyContent: 'center'
+
+  },
+  
+  
+});
+
+export default CustomDrawerContent;
